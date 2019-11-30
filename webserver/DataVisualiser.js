f16dd3c626de16bb7a621d9512e869882e8b8442
Oli Thompson <olithompson@rocketmail.com>
2019-11-30 19:15:42 +0000

    Update DataVisualiser.js

--- webserver/DataVisualiser.js
+++ webserver/DataVisualiser.js
@@ -1,9 +1,12 @@
 console.log("running")
+
+
 function parseData(createGraph) {
 	Papa.parse("emotion_data.csv", {
+    download: true,
 		complete: function(results) {
-			console.log(results.data);
-      createGraph(results.data);
+			   console.log(results.data);
+         createGraph(results.data);
 		}
 	});
 }
