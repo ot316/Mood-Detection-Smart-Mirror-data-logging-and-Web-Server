2f3dc7d3d0ff4620c9fef2dbf44bc20c200eb6d3
Oli Thompson <olithompson@rocketmail.com>
2019-11-30 19:35:34 +0000

    Update DataVisualiser.js

--- webserver/DataVisualiser.js
+++ webserver/DataVisualiser.js
@@ -12,7 +12,15 @@ function parseData(createGraph) {
 }
 
 function createGraph(data) {
+  var time = [];
+  var emotions = [];
 
+  for (var i = 0; i < data/length; i++) {
+    console.log(data[i]);
+  }
+  var chart = c3.generate({
+      bindto: '#chart',
+  })
 }
 
 parseData(createGraph);
