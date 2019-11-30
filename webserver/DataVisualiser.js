aa89bf281512c67f291ce0a7278aa753e1eeb2a6
Oli Thompson <olithompson@rocketmail.com>
2019-11-30 18:41:05 +0000

    chart

--- webserver/DataVisualiser.js
+++ webserver/DataVisualiser.js
@@ -1,19 +1,114 @@
 console.log("running")
-var chart = new Chart('chart', {
-  type: 'horizontalBar',
-  data: {
-    labels: ['A', 'B', 'C'],
-    datasets: [
-      {
-        data: [10, 20, 30]
+$(document).ready(function() {
+
+  var TITLE = 'Mood Over Past Week';
+
+  // Which column names contain data points?
+  var Y_LABELS = ['Angry', 'Disgust', 'Happy', 'Sad', 'Surprise'];
+
+  // Which column name contains names (labels) for each series?
+  var NAMES = 'district';
+
+  // Optionally, which column contains color names for the series?
+  // If not specified, will apply default color scheme
+  var COLORS = 'color';
+
+  var X_AXIS = 'Time';  // x-axis label and label in tooltip
+  var Y_AXIS = 'Emotion'; // y-axis label and label in tooltip
+
+  var SHOW_GRID = true; // `true` to show the grid, `false` to hide
+  var SHOW_LEGEND = true; // `true` to show the legend, `false` to hide
+
+  // Read data file and create a chart
+  d3.csv('emotion_data.csv').then(function(rows) {
+
+    var datasets = rows.map(function(row) {
+
+      var dataset = {
+        label: row[NAMES],
+        fill: false,
+        data: X_LABELS.map(function(t) { return row[t] })
       }
-    ]
-  }
+
+      if (row[COLORS]) {
+        dataset.backgroundColor = row[COLORS];
+        dataset.borderColor = row[COLORS];
+        dataset.pointBackgroundColor = row[COLORS];
+        dataset.pointBorderColor = row[COLORS];
+      }
+
+      return dataset;
+
+    });
+
+		var lineChartData = {
+      labels: X_LABELS,
+			datasets: datasets
+    };
+
+    var ctx = document.getElementById('container').getContext('2d');
+
+    new Chart(ctx, {
+      type: 'line',
+      data: lineChartData,
+
+      options: {
+        title: {
+          display: true,
+          text: TITLE,
+          fontSize: 14,
+        },
+        legend: {
+          display: SHOW_LEGEND,
+        },
+        scales: {
+          xAxes: [{
+            scaleLabel: {
+              display: X_AXIS !== '',
+              labelString: X_AXIS
+            },
+            gridLines: {
+              display: SHOW_GRID,
+            },
+            ticks: {
+              callback: function(value, index, values) {
+                return value.toLocaleString();
+              }
+            }
+          }],
+          yAxes: [{
+            beginAtZero: true,
+            scaleLabel: {
+              display: Y_AXIS !== '',
+              labelString: Y_AXIS
+            },
+            gridLines: {
+              display: SHOW_GRID,
+            },
+            ticks: {
+              callback: function(value, index, values) {
+                return value.toLocaleString()
+              }
+            }
+          }]
+        },
+        tooltips: {
+          displayColors: false,
+          callbacks: {
+            label: function(tooltipItem, all) {
+              return all.datasets[tooltipItem.datasetIndex].label
+                + ': ' + tooltipItem.yLabel.toLocaleString();
+            }
+          }
+        },
+        plugins: {
+          colorschemes: {
+            scheme: 'brewer.Paired12'
+          }
+        }
+      }
+    });
+
+  });
+
 });
-
-d3.csv('emotion_data.csv')
-  .then(makeChart);
-
-function makeChart(players) {
-  // players is an array of objects where each object represents a player
-}
