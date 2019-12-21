5e276f0898d8d37aff55faea915a7fbe36f2275f
Oli Thompson <olithompson@rocketmail.com>
2019-12-21 22:14:42 +0000

    Update DataVisualiser.js

--- webserver/DataVisualiser.js
+++ webserver/DataVisualiser.js
@@ -31,7 +31,7 @@ function createGraph(data) {
 
         var ctx = document.getElementById('myChart').getContext('2d');
         var myChart = new Chart(ctx, {
-            type: 'bar',
+            type: 'line',
             data: {
                 labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
                 datasets: [{
