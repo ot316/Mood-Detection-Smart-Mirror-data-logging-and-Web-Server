fa3dd6551e0df19343bcd82d3433320323c5af74
Oli Thompson <olithompson@rocketmail.com>
2019-12-22 10:16:30 +0000

    Update DataVisualiser.js

--- webserver/DataVisualiser.js
+++ webserver/DataVisualiser.js
@@ -1,16 +1,16 @@
 console.log("running")
+var ctx = document.getElementById('myChart').getContext('2d');
 
-
-function parseData(createGraph) {
-    Papa.parse("emotion_data.csv", {
-        download: true,
+function parseData(process) {
+    var data = Papa.parse("emotion_data.csv", {
+        worker: true,
         complete: function(results) {
-            createGraph(results.data);
+            process(results.data);
         }
     });
 }
 
-function createGraph(data) {
+function process(data) {
     var datetime = [];
     var angry = ["Angry"];
     var disgust = ["Disgust"];
@@ -27,47 +27,46 @@ function createGraph(data) {
         sad.push(data[i][5]);
         surprise.push(data[i][6]);
         maximumpredictedemotion.push(data[i][8])
-        console.log(data[i][8]);
+        console.log("data processed");
     }
+
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
 
 }
 
 parseData(createGraph);
-
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
