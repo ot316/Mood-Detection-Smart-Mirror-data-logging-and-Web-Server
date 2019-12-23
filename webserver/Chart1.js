a02299bb2c272bc553f469bbd7a0f7d7988b737f
Oli Thompson <olithompson@rocketmail.com>
2019-12-23 11:53:52 +0000

    change datapoints declaration

--- webserver/Chart1.js
+++ webserver/Chart1.js
@@ -9,7 +9,7 @@ window.setInterval(function() {
 
 function setup1() {
     var datapoints = parseFloat(document.getElementById("userinput1").value);
-    if (typeof datapoints != 'number') {
+    if (typeof datapoints == 'undefined') {
         var datapoints = 100;
     }
     console.log(datapoints);
