b49b1f3e2972fe4fdd8fdb19d349e4b3e250e95b
ot316 <olithompson@rocketmail.com>
2020-03-05 20:07:07 +0000

    add main.copy

---
+++ main - Copy.py
@@ -0,0 +1,89 @@
+import os
+import sys
+import cv2
+import datetime
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+from keras.models import model_from_json
+from keras.preprocessing import image
+from vis.visualization import visualize_cam,overlay
+
+#load model
+model = model_from_json(open("model.json", "r").read())
+
+#load weights
+model.load_weights('model_weights.h5')
+
+#initialise face detection
+face_haar_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+cap=cv2.VideoCapture(0)
+count = 0
+width = 48
+height = 48
+
+arguments = sys.argv
+
+
+if len(arguments) == 2:
+    if arguments[1] == "-show":
+        print("Running with video feed, press 'q' key to exit.")
+
+ret,test_img=cap.read()  # captures frame and returns boolean value and captured image
+if not ret:
+    print("camera error")
+
+gray_img= cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)
+
+
+#use haar cascades to ientify faces
+faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
+
+#convert detected face into correct format for the CNN
+for (x,y,w,h) in faces_detected:
+    cv2.rectangle(test_img,(x,y),(x+w,y+h),(255,0,255),thickness=1)
+    face=gray_img[y:y+w,x:x+h] #cropping region of interest 
+    face=cv2.resize(face,(width,height)) #resize to 48 * 48
+    img_pixels = image.img_to_array(face) #create pixel array
+    img_pixels = np.expand_dims(img_pixels, axis = 0) 
+    #img_pixels = (img_pixels-min(img_pixels))/(max(img_pixels)-min(img_pixels))
+    img_pixels /= 255
+    
+    #feed image data into CNN
+    predictions = model.predict(img_pixels)
+
+    #find max indexed array
+    max_index = np.argmax(predictions[0])
+
+    #classifications
+    emotions = ('angry', 'disgust', 'neutral', 'happy', 'sad', 'surprise', 'neutral')
+    
+    predicted_emotion = emotions[max_index]
+    
+    if predicted_emotion and predicted_emotion != 'neutral':
+        data = {"DateTime": datetime.datetime.now(), "Angry": predictions[0,0], "Disgust": predictions[0,1], "Happy": predictions[0,3], "Sad": predictions[0,4], "Surprise": predictions[0,5], "Neutral": predictions[0,5], "Maximum Predicted Emotion": predicted_emotion}
+        df = df.append(data, ignore_index=True)
+        print(predicted_emotion)
+        cv2.putText(test_img, predicted_emotion, (int(x), int(y-10)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 2)
+        count+=1
+            
+    if count == 10:
+        df.to_csv("webserver/emotion_data.csv")
+        print("csv file saved")
+        count = 0
+
+
+resized_img = cv2.resize(test_img, (1000, 700))
+cv2.imshow('Facial emotion analysis ',resized_img)
+
+grads = visualize_cam(model, 4, None, test_image, penultimate_layer_idx=None, \
+    backprop_modifier=None, grad_modifier=None)
+
+jet_heatmap = np.uint8(plt.jet(grads)[..., :3] * 255)
+ax.imshow(overlay(jet_heatmap, test_img))
+
+
+
+cap.release()
+cv2.destroyAllWindows
